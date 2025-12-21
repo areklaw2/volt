2cbabc8b5eea468d61e7928bbd3c85aabf35566b
Aza Walker <azawalker.jr@gmail.com>
2025-12-21 00:28:12 -0500

    chatting working

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-use axum::{Router, routing::any};
-use volt::ws_handler;
-
-use std::net::SocketAddr;
-use tower_http::trace::{DefaultMakeSpan, TraceLayer};
-
+use axum::{
+    Router,
+    extract::{
+        State,
+        ws::{Message, Utf8Bytes, WebSocket, WebSocketUpgrade},
+    },
+    response::IntoResponse,
+    routing::get,
+};
+use futures_util::{sink::SinkExt, stream::StreamExt};
+use std::{
+    collections::HashSet,
+    sync::{Arc, Mutex},
+};
+use tokio::sync::broadcast;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+struct AppState {
+    user_set: Mutex<HashSet<String>>,
+    tx: broadcast::Sender<String>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -17,20 +31,88 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let app = Router::new().route("/ws", any(ws_handler)).layer(
-        TraceLayer::new_for_http()
-            .make_span_with(DefaultMakeSpan::default().include_headers(false)),
-    );
+    let user_set = Mutex::new(HashSet::new());
+    let (tx, _rx) = broadcast::channel(100);
+    let app_state = Arc::new(AppState { user_set, tx });
+
+    let app = Router::new()
+        .route("/ws", get(ws_handler))
+        .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
-
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(
-        listener,
-        app.into_make_service_with_connect_info::<SocketAddr>(),
-    )
-    .await
-    .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn ws_handler(ws: WebSocketUpgrade, State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    ws.on_upgrade(move |socket| handle_socket(socket, state))
+}
+
+async fn handle_socket(stream: WebSocket, state: Arc<AppState>) {
+    let (mut sender, mut receiver) = stream.split();
+
+    let mut username = String::new();
+    while let Some(Ok(message)) = receiver.next().await {
+        if let Message::Text(name) = message {
+            check_username(&state, &mut username, name.as_str());
+
+            if !username.is_empty() {
+                break;
+            } else {
+                let _ = sender
+                    .send(Message::Text(Utf8Bytes::from_static(
+                        "Username already taken.",
+                    )))
+                    .await;
+
+                return;
+            }
+        }
+    }
+
+    let mut rx = state.tx.subscribe();
+
+    let msg = format!("{username} joined.");
+    tracing::debug!("{msg}");
+    let _ = state.tx.send(msg);
+
+    let mut send_task = tokio::spawn(async move {
+        while let Ok(msg) = rx.recv().await {
+            if sender.send(Message::text(msg)).await.is_err() {
+                break;
+            }
+        }
+    });
+
+    let tx = state.tx.clone();
+    let name = username.clone();
+
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(Message::Text(text))) = receiver.next().await {
+            let _ = tx.send(format!("{name}: {text}"));
+        }
+    });
+
+    tokio::select! {
+        _ = &mut send_task => recv_task.abort(),
+        _ = &mut recv_task => send_task.abort(),
+    };
+
+    let msg = format!("{username} left.");
+    tracing::debug!("{msg}");
+    let _ = state.tx.send(msg);
+
+    state.user_set.lock().unwrap().remove(&username);
+}
+
+fn check_username(state: &AppState, string: &mut String, name: &str) {
+    let mut user_set = state.user_set.lock().unwrap();
+
+    if !user_set.contains(name) {
+        user_set.insert(name.to_owned());
+
+        string.push_str(name);
+    }
 }
