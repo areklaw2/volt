a06ee9dbaafb6d69a989b8beac2c5d05b15ec224
Aza Walker <azawalker.jr@gmail.com>
2025-12-23 21:07:49 -0500

    frontend change to react

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
-import path from 'node:path';
-import { defineConfig } from 'vite';
+import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
