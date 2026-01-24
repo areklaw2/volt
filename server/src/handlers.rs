aad72613afc44fe7ee3151125a682e396221647e
Aza Walker <azawalker.jr@gmail.com>
2026-01-24 05:00:48 -0500

    changing port

--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -52,7 +52,7 @@ pub fn configure_http_routes() -> Router<Arc<AppState>> {
         .layer(
             CorsLayer::new()
                 .allow_headers([http::header::CONTENT_TYPE])
-                .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+                .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
                 .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::PUT, Method::DELETE]),
         )
 }
