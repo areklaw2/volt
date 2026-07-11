2a638c8bf0263ca672852c1887176ed94bb629ba
Aza Walker <azawalker.jr@gmail.com>
2026-07-11 02:30:32 -0400

    refactor: scaffold

---
+++ server/src/domain.rs
@@ -0,0 +1,7 @@
+pub mod conversation;
+pub mod errors;
+pub mod events;
+pub mod ids;
+pub mod message;
+pub mod repository;
+pub mod user;
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,7 +1,10 @@
+pub mod application;
 pub mod config;
+pub mod domain;
 pub mod dto;
 pub mod errors;
 pub mod handlers;
+pub mod infrastructure;
 pub mod repositories;
 
 use secrecy::ExposeSecret;
